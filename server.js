8a9127386bc42ba67bacc17fc293a6b556c7f94a
Graciela <ariwijayagraciela@gmail.com>
2025-02-02 15:48:54 -0800

    updated server.js again to troubleshoot Render hosting

--- server.js
+++ server.js
@@ -1,59 +1,25 @@
 const http = require("http");
 const url = require("url");
-const getDate = require("./modules/utils.js"); // Import the getDate function
-const messages = require("./lang/en/en.js"); // Import user-facing messages
+const getDate = require("./modules/utils").getDate;
+const messages = require("./lang/en/en");
 
-const PORT = process.env.PORT || 3000; // Use Render's assigned port
+const PORT = process.env.PORT || 3000; // Use Render's assigned port or default to 3000
 
 const server = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true);
     const pathname = parsedUrl.pathname;
     const query = parsedUrl.query;
 
-    if (pathname === "/getDate") {
-        const name = query.name || "Guest";
-        const message = `<p style="color:blue">${messages.greeting.replace("%1", name)} ${getDate()}</p>`;
+    if (pathname === "/getDate" && query.name) {
+        const responseMessage = `<p style="color:blue;">${messages.greeting.replace("%1", query.name)} ${getDate()}</p>`;
         res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(message);
-    } else if (pathname === "/writeFile") {
-        const fs = require("fs");
-        const text = query.text;
-
-        if (!text) {
-            res.writeHead(400, { "Content-Type": "text/plain" });
-            res.end("Error: No text provided to write.");
-            return;
-        }
-
-        fs.appendFile("file.txt", text + "\n", (err) => {
-            if (err) {
-                res.writeHead(500, { "Content-Type": "text/plain" });
-                res.end("Error writing to file.");
-            } else {
-                res.writeHead(200, { "Content-Type": "text/plain" });
-                res.end("Successfully wrote to file.");
-            }
-        });
-    } else if (pathname.startsWith("/readFile")) {
-        const fs = require("fs");
-        const filename = pathname.replace("/readFile/", "");
-
-        fs.readFile(filename, "utf8", (err, data) => {
-            if (err) {
-                res.writeHead(404, { "Content-Type": "text/plain" });
-                res.end(`Error 404: File '${filename}' not found.`);
-            } else {
-                res.writeHead(200, { "Content-Type": "text/plain" });
-                res.end(data);
-            }
-        });
+        res.end(responseMessage);
     } else {
-        res.writeHead(404, { "Content-Type": "text/html" });
-        res.end("<h1>404 Not Found</h1>");
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("404 Not Found");
     }
 });
 
-// Start the server
 server.listen(PORT, () => {
     console.log(`Server running at http://0.0.0.0:${PORT}/`);
 });
